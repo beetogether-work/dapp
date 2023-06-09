64485fe4a97d53a0257c20e6d5353d443b1caee8
0xRomain <romain@talentlayer.org>
2023-06-09 21:14:05 +0200

    init invite page

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -8,7 +8,7 @@ function Onboarding() {
       <div className='h-screen md:flex'>
         <LeftSide
           title='Work together easly'
-          subtext="You're in the perfect place to start your new collective, we have all the tools to growyour business and collaborate effectively."></LeftSide>
+          subtext="You're in the perfect place to start your new collective, you will have here all the tools to grow your business and collaborate effectively."></LeftSide>
 
         <div className='flex flex-col items-center justify-between py-10 md:w-1/2'>
           <div className='mx-auto flex w-full max-w-xs items-center justify-between'>
--- src/pages/onboarding/invite.tsx
+++ src/pages/onboarding/invite.tsx
@@ -1,5 +1,32 @@
+import HiveInviteForm from '../../components/Form/HiveInviteForm';
+import Logo from '../../components/Layout/Logo';
+import LeftSide from '../../components/onboarding/LeftSide';
+
 function Invite() {
-  return <h1>Join this hive!</h1>;
+  return (
+    <div>
+      <div className='h-screen md:flex'>
+        <LeftSide
+          title='Work together easly'
+          subtext="You'have invited to be part of a new collective. You will have here all the tools to grow your business and collaborate effectively."></LeftSide>
+
+        <div className='flex flex-col items-center justify-between py-10 md:w-1/2'>
+          <div className='mx-auto flex w-full max-w-xs items-center justify-between'>
+            <a href='/' className='hover:text-primary-500 transition-colors duration-300 px-2'>
+              <Logo />
+            </a>
+          </div>
+          <HiveInviteForm />
+
+          <div className='text-center'>
+            <p className='font-sans text-sm font-normal leading-normal text-gray-400 mt-10'>
+              © 2023 BeeTogether. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Invite;
